cc03520eb9a5183b7f40a0ee7580ca3a42f06cc4
willchenxa <43488067+willchenxa@users.noreply.github.com>
2023-02-02 19:27:20 +1000

    Add mediatR validation (#4)

--- BuberDinner.Application/DependencyInjection.cs
+++ BuberDinner.Application/DependencyInjection.cs
@@ -1,5 +1,11 @@
 using Microsoft.Extensions.DependencyInjection;
 using MediatR;
+using BuberDinner.Application.Authentication.Commands.Register;
+using ErrorOr;
+using BuberDinner.Application.Authentication.Common;
+using BuberDinner.Application.Common.Behaviors;
+using FluentValidation;
+using System.Reflection;
 
 namespace BuberDinner.Application;
 
@@ -8,6 +14,13 @@ public static class DependencyInjection
  public static IServiceCollection AddApplication(this IServiceCollection services)
  {
   services.AddMediatR(typeof(DependencyInjection).Assembly);
+
+        services.AddScoped(typeof(IPipelineBehavior<,>),
+                           typeof(ValidationBehavior<,>));
+        
+        //services.AddScoped<IPipelineBehavior<RegisterCommand, ErrorOr<AuthenticationResult>>, ValidationRegisterCommandBehavior>();
+
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
   // services.AddScoped<IAuthenticationCommandService, AuthenticationCommandService>();
   // services.AddScoped<IAuthenticationQueryService, AuthenticationQueryService>();
   return services;

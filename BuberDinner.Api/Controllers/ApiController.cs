ae441f467005e36b4fa28856734dd1a2e88ad688
William Chen <will.chenxa@gmail.com>
2023-02-02 19:26:35 +1000

    Add mediatR validation

--- BuberDinner.Api/Controllers/ApiController.cs
+++ BuberDinner.Api/Controllers/ApiController.cs
@@ -1,6 +1,7 @@
 using BuberDinner.Api.Common.Http;
 using ErrorOr;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace BuberDinner.Api.Controllers;
 
@@ -8,19 +9,45 @@ namespace BuberDinner.Api.Controllers;
 public class ApiController : ControllerBase
 {
  protected IActionResult Problem(List<Error> errors)
- {
-  HttpContext.Items[HttpContextItemKeys.Errors] = errors;
+    {
+        if (errors.Count is 0)
+        {
+            return Problem();
+        }
 
-  var firstError = errors[0];
+        if (errors.All(error => error.Type == ErrorType.Validation))
+        {
+            return ValidationProblem(errors);
+        }
 
-  var statusCode = firstError.Type switch
-  {
-   ErrorType.Conflict => StatusCodes.Status409Conflict,
-   ErrorType.Validation => StatusCodes.Status400BadRequest,
-   ErrorType.NotFound => StatusCodes.Status404NotFound,
-   _ => StatusCodes.Status500InternalServerError
-  };
+        HttpContext.Items[HttpContextItemKeys.Errors] = errors;
 
-  return Problem(statusCode: statusCode, title: firstError.Description);
- }
+
+        return Problem(errors[0]);
+    }
+
+    private IActionResult Problem(Error firstError)
+    {
+        var statusCode = firstError.Type switch
+        {
+            ErrorType.Conflict => StatusCodes.Status409Conflict,
+            ErrorType.Validation => StatusCodes.Status400BadRequest,
+            ErrorType.NotFound => StatusCodes.Status404NotFound,
+            _ => StatusCodes.Status500InternalServerError
+        };
+
+        return Problem(statusCode: statusCode, title: firstError.Description);
+    }
+
+    private IActionResult ValidationProblem(List<Error> errors)
+    {
+        var modelStateDictionary = new ModelStateDictionary();
+
+        foreach (var error in errors)
+        {
+            modelStateDictionary.AddModelError(error.Code,
+                                               error.Description);
+        }
+        return ValidationProblem(modelStateDictionary);
+    }
 }
---
+++ BuberDinner.Application/Authentication/Queries/Login/LoginQueryValidator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using FluentValidation;
+
+namespace BuberDinner.Application.Authentication.Queries.Login;
+
+public class LoginQueryValidator : AbstractValidator<LoginQuery>
+{
+	public LoginQueryValidator()
+	{
+		RuleFor(x => x.Email).NotEmpty();
+		RuleFor(x => x.Password).NotEmpty();
+	}
+}
+
